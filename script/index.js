c200a46dda126565a8b4352ee3b10e49b1d6e04c
buck80 <mkomljenovic11@gmail.com>
2022-06-01 20:31:36 +0200

    adding and styling menu, mobile version

--- script/index.js
+++ script/index.js
@@ -1,1 +1,10 @@
-console.log("here");
+const ext = document.querySelector(".close");
+const menu = document.querySelector(".menu");
+const nav = document.querySelector("nav");
+
+menu.addEventListener("click", () => {
+  nav.classList.add("open-nav");
+});
+ext.addEventListener("click", () => {
+  nav.classList.remove("open-nav");
+});
